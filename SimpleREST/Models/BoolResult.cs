98586a3a48f36a9cdb968b0a9f8cd8a113e27e24
Teerasej <teerasej@gmail.com>
2017-07-04 00:03:43 +0700

    Create to represent a custom object

---
+++ SimpleREST/Models/BoolResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SimpleREST.Models
+{
+    public class BoolResult
+    {
+        public BoolResult()
+        {
+        }
+
+        public bool Result
+        {
+            get;
+            set;
+        }
+    }
+}
